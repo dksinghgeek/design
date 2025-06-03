66288d8dfa280a58147f8b44a4c01dd5551e5a9c
Dharmendra Singh <dksingh.work@outlook.com>
2025-06-03 09:39:19 +0530

    added documentation

--- src/main/java/com/ggn/probs/design/patterns/observer/Demo.java
+++ src/main/java/com/ggn/probs/design/patterns/observer/Demo.java
@@ -2,6 +2,7 @@ package com.ggn.probs.design.patterns.observer;
 
 public class Demo {
 
+	//Notify Me Functionality
 	public static void main(String[] args) {
 		StockObserable observable = new IPhoneStockObserableImpl();
 		NotificationAlertObserver email = new EmailAlertObserverImpl("abc@gmail.com", observable);
